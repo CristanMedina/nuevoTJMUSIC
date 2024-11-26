bc012645bcf897d036a9b17ee98567b12f008ec2
Cristan <crishighlights@gmail.com>
2024-11-25 21:35:16 -0800

    [Agregado] rutas para los endpoints de los posts. Ya se probo y todos los endpoints funcionan debidamente

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,10 +1,21 @@
 import express from 'express';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, getPostById, getPosts, getPostsByUser, deletePost, updatePost, commentPost, replyComment, likePost } from '../controllers/post.controller.js';
 import { checkAuthMiddleware } from "../controllers/auth.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
 
-router.post('/createPost', verifyToken, checkAuthMiddleware, createPost);
+router.get('/', getPosts);
+router.get('/:id', getPostById);
+
+router.post('/', verifyToken, checkAuthMiddleware, createPost);
+router.put('/:id', verifyToken, checkAuthMiddleware, updatePost);
+router.delete('/:id', verifyToken, checkAuthMiddleware, deletePost);
+
+router.post('/:id/comments', verifyToken, checkAuthMiddleware, commentPost);
+router.post('/:id/comments/:commentId/replies', verifyToken, checkAuthMiddleware, replyComment);
+router.post('/:id/like', verifyToken, checkAuthMiddleware, likePost);
+
+router.get('/user/:userId', verifyToken, checkAuthMiddleware, getPostsByUser);
 
 export default router;
